9e943a569317d6e31b6220e644351d2832e68ecc
Serhii Lisovyk <serlesovik@gmail.com>
2025-09-23 22:45:11 +0300

    Update docker

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,7 +21,7 @@ connectDB()
 const PORT = process.env.PORT || 5000
 
 try {
-  app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+  app.listen(+PORT, '0.0.0.0', () => console.log(`Server started on port ${PORT}`))
 } catch (error) {
   console.error('Error starting the server:', error)
 }
